244fbf2bb7e515c0c84da30eb53d873dc522c159
Arindashiphura <ashiphura@gmail.com>
2024-10-20 23:16:26 +0300

    tdd project

---
+++ test/multiply.js
@@ -0,0 +1,21 @@
+// function multiply(a, b) {
+//     return 1;
+//   }
+
+
+//   function multiply(a, b) {
+//     return a * b;
+//   }
+  
+//   function multiply(a, b) {
+//     return a * b;
+//   }
+  
+  
+  
+//   module.exports = multiply;
+  
+
+export default function multiply(a, b) {
+    return a * b;
+}
